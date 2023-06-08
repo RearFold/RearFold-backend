1a1e7c6b680a2c64fe81bf5421e3af97692b2d1c
Nega0619 <80737049+Nega0619@users.noreply.github.com>
2023-06-09 05:03:02 +0900

    add select insert 추가

--- temp/database.py
+++ temp/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Boolean, Integer, Float, DateTime, Sequence, TIMESTAMP, VARCHAR, Text
 
 db = create_engine('postgresql://hwi:1895@localhost:5432/rearfold',
                    connect_args={'options': '-csearch_path=backend'})
-Base = declarative_base()
+Base =   declarative_base()
 
 Session = sessionmaker(bind=db)
 session = Session()
@@ -75,6 +75,22 @@ from sqlalchemy import select
 stmt = select(Admin).where(Admin.admin_id == 'root')
 
 # execution
-rs = session.execute(stmt) # returns model object
+rs = session.execute(stmt).fetchall() # returns model object
+admin = rs[0][0]  # 첫 번째 튜플의 첫 번째 요소인 Admin 객체에 접근합니다.
+
+admin_id = admin.admin_id
+admin_pw = admin.admin_pw
+created_at = admin.created_at
+description = admin.description
+
+# 데이터 사용
+print(admin_id, admin_pw, created_at, description)
 
 
+
+from sqlalchemy import insert
+
+# insert method
+# stmt = (insert(Admin).values(admin_id='tired', admin_pw='1234'))
+# rs = session.execute(stmt)
+session.commit()
